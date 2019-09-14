f5cc081f50a65b3fcf8182331e229e02a49df464
sambit <sambitdas1406@gmail.com>
2019-09-14 12:27:11 +0530

    firebase added to apps

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyC-AbaPNXuPjweE2t1kcw6nfN6OF0nPujc",
+    authDomain: "eshop-4b43c.firebaseapp.com",
+    databaseURL: "https://eshop-4b43c.firebaseio.com",
+    projectId: "eshop-4b43c",
+    storageBucket: "",
+    messagingSenderId: "572967079962",
+    appId: "1:572967079962:web:36d69c06e59e0fa43741a3"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyC-AbaPNXuPjweE2t1kcw6nfN6OF0nPujc",
+    authDomain: "eshop-4b43c.firebaseapp.com",
+    databaseURL: "https://eshop-4b43c.firebaseio.com",
+    projectId: "eshop-4b43c",
+    storageBucket: "",
+    messagingSenderId: "572967079962",
+    appId: "1:572967079962:web:36d69c06e59e0fa43741a3"
+  }
 };
 
 /*
